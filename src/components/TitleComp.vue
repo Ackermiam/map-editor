c14dcc1fb1779d8aea66acac06ee24f010af7300
unknown <67873431+Ackermiam@users.noreply.github.com>
2024-11-04 19:49:44 +0100

    delete useless things

---
+++ src/components/TitleComp.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <h1>Map Editor {{ JSONINFO.version }}</h1>
+</template>
+
+<script setup lang="ts">
+import JSONINFO from '../../package.json'
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
