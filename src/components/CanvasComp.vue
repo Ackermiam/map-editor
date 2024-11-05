667914529e7651e80f50f2f98a329c23c279e911
unknown <67873431+Ackermiam@users.noreply.github.com>
2024-11-05 14:20:43 +0100

    download image 400x400

--- src/components/CanvasComp.vue
+++ src/components/CanvasComp.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
+  <div class="CanvasComp">
     <div>
       <button
         @click="
           () => {
-            color = 'blue';
+            color = 'turquoise';
           }
         "
       >
-        blue
+        eau
       </button>
       <button
         @click="
@@ -17,20 +17,29 @@
           }
         "
       >
-        green
+        herbe
       </button>
       <button
         @click="
           () => {
-            color = 'brown';
+            color = 'sienna';
           }
         "
       >
-        brown
+        terre
       </button>
-      <button @click="downloadMapImage()">Télécharger Map</button>
+      <button
+        @click="
+          () => {
+            color = 'grey';
+          }
+        "
+      >
+        pierre
+      </button>
     </div>
     <canvas id="mapEditor" width="400" height="400"></canvas>
+    <button @click="downloadCanvasImage()" class="download">Télécharger Map</button>
   </div>
 </template>
 
@@ -39,26 +48,24 @@ import { onMounted, ref } from "vue";
 
 const color = ref("blue");
 
-const mapCanvas = document.createElement("canvas");
-mapCanvas.width = 16;
-mapCanvas.height = 16;
-const mapCtx = mapCanvas.getContext("2d");
-
-let originalCanvas;
+let canvas;
 let ctx;
 const gridSize = 25;
 
+const blocks = [];
+
 const drawGrid = () => {
-  for (let x = 0; x <= originalCanvas.width; x += gridSize) {
+  ctx.strokeStyle = "#ddd";
+  for (let x = 0; x <= canvas.width; x += gridSize) {
     ctx.beginPath();
     ctx.moveTo(x, 0);
-    ctx.lineTo(x, originalCanvas.height);
+    ctx.lineTo(x, canvas.height);
     ctx.stroke();
   }
-  for (let y = 0; y <= originalCanvas.height; y += gridSize) {
+  for (let y = 0; y <= canvas.height; y += gridSize) {
     ctx.beginPath();
     ctx.moveTo(0, y);
-    ctx.lineTo(originalCanvas.width, y);
+    ctx.lineTo(canvas.width, y);
     ctx.stroke();
   }
 };
@@ -66,72 +73,62 @@ const drawGrid = () => {
 const drawSquare = (x, y) => {
   const gridX = Math.floor(x / gridSize) * gridSize;
   const gridY = Math.floor(y / gridSize) * gridSize;
+
   ctx.fillStyle = color.value;
   ctx.fillRect(gridX, gridY, gridSize, gridSize);
+
+  blocks.push({ x: gridX, y: gridY, color: color.value });
 };
 
-const generateMapImage = () => {
-  for (let y = 0; y < 16; y++) {
-    for (let x = 0; x < 16; x++) {
-      const imageData = ctx.getImageData(x * 25, y * 25, 25, 25);
-      const color = averageColor(imageData);
-
-      if (!(color.r === 0 && color.g === 0 && color.b === 0)) {
-        mapCtx.fillStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
-        mapCtx.fillRect(x, y, 1, 1);
-      }
-    }
-  }
-
-  const mapImage = mapCanvas.toDataURL();
-  console.log(mapImage);
-  return mapImage;
+const redrawBlocks = () => {
+  blocks.forEach(block => {
+    ctx.fillStyle = block.color;
+    ctx.fillRect(block.x, block.y, gridSize, gridSize);
+  });
 };
 
-const averageColor = (imageData) => {
-  const data = imageData.data;
-  let r = 0,
-    g = 0,
-    b = 0;
-  for (let i = 0; i < data.length; i += 4) {
-    r += data[i];
-    g += data[i + 1];
-    b += data[i + 2];
-  }
-  const pixelCount = data.length / 4;
-  return {
-    r: Math.round(r / pixelCount),
-    g: Math.round(g / pixelCount),
-    b: Math.round(b / pixelCount),
-  };
-};
+const downloadCanvasImage = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-const downloadMapImage = () => {
-  const mapImage = generateMapImage();
+  redrawBlocks();
 
+  const image = canvas.toDataURL();
   const downloadLink = document.createElement("a");
-  downloadLink.href = mapImage;
-  downloadLink.download = "map16x16.png";
-
+  downloadLink.href = image;
+  downloadLink.download = "map400x400.png";
   downloadLink.click();
+
+  drawGrid();
 };
 
 onMounted(() => {
-  originalCanvas = document.querySelector("canvas");
-  ctx = originalCanvas.getContext("2d");
+  canvas = document.querySelector("canvas");
+  ctx = canvas.getContext("2d");
+
   drawGrid();
 
-  originalCanvas.addEventListener("click", (event) => {
-    const rect = originalCanvas.getBoundingClientRect();
+  canvas.addEventListener("click", event => {
+    const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
     drawSquare(x, y);
+    drawGrid();
   });
 });
 </script>
 
 <style scoped>
+.CanvasComp {
+  display: flex;
+  flex-direction: column;
+}
+
 #mapEditor {
   border: 6px solid #213547;
+  margin-bottom: 2em;
+}
+
+button {
+  margin: 0 1em 2em 0;
 }
 </style>
